7d927376b44e6fa809f68e793df3caadc62ccd8d
IveryChen <ivy_chen1@brown.edu>
2024-03-15 18:06:02 -0400

    adding things

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import BakeryItem from "./components/BakeryItem.js"
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
 
@@ -9,21 +10,55 @@ bakeryData.forEach((item) => {
 /* ############################################################## */
 
 function App() {
-  // TODO: use useState to create a state variable to hold the state of the cart
-  /* add your cart state code here */
+  const [cart, setCart] = useState([])
+  const [itemMap, setItemMap] = useState({});
+
+  const addToCart = (item) =>{
+   setCart([...cart, item]);
+
+   setItemMap((prevMap) => ({
+    ...prevMap,
+    [item.name]: (prevMap[item.name] || 0) + 1,
+  }));
+  }
+
+  const totalPrice = cart.reduce(
+    (totalPrice, item) => totalPrice + item.price,
+    0
+   );
 
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+      <h1>Blueno's Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
+      <div className="main-page">
+        <div className="all-cards">
+          {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
+            <BakeryItem
+            image={item.image}
+            name={item.name}
+            description={item.description}
+            price={item.price}
+            addToCart={()=> addToCart(item)}
+            >
+            </BakeryItem>
+          ))}
+        </div>
 
-      <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        <div className="cart">
+          <h2>My Cart</h2>
+          
+            {Object.keys(itemMap).map((itemName, index) => (
+              <p key={index}>
+                {itemMap[itemName]} x {itemName}
+              </p>
+            ))}
+         
+          <p> Total: $ {totalPrice.toFixed(2)}</p>
+        </div>
       </div>
+
+
     </div>
   );
 }
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,12 @@
 // TODO: create a component that displays a single bakery item
+export default function BakeryItem(props){
+    return(
+        <div className="card">
+            <img src={props.image}></img>
+            <p>{props.name}</p>
+            <p>{props.description}</p>
+            <p>${props.price}</p> 
+            <button onClick={props.addToCart}>Add to cart</button>
+        </div>
+    );
+}
